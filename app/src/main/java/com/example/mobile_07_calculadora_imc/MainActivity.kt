a1b866c7371b3c7d2e44dacdf62fed34589ba943
EDVAN ALVES DE OLIVEIRA <25132698@dsn127m1246731.cfp127aluno.local>
2026-03-11 09:02:50 -0300

    feat: add math operations and display the results

--- app/src/main/java/com/example/mobile_07_calculadora_imc/MainActivity.kt
+++ app/src/main/java/com/example/mobile_07_calculadora_imc/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mobile_07_calculadora_imc
 
 import android.R.attr.contentDescription
 import android.R.attr.label
+import android.R.attr.onClick
 import android.R.attr.singleLine
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +11,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,7 +22,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.OutlinedTextField
@@ -40,6 +45,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontVariation.weight
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -65,9 +71,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun IMCScreen(name: String, modifier: Modifier = Modifier) {
     var heightField by remember {
-        mutableStateOf(0)
+        mutableStateOf("")
     }
     var weightField by remember {
+        mutableStateOf("")
+    }
+    var imcValue by remember {
+        mutableStateOf("")
+    }
+    var imcMessage by remember {
         mutableStateOf("")
     }
     Column(modifier = modifier.fillMaxSize()) {
@@ -92,19 +104,25 @@ fun IMCScreen(name: String, modifier: Modifier = Modifier) {
                 fontWeight = FontWeight.Bold
             )
         }
-        Column( modifier = Modifier.fillMaxWidth()
-            .padding(horizontal = 36.dp)) {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .offset(y = (-30.dp))
-                    .height(300.dp),
-                colors = CardDefaults.cardColors(
-                    containerColor = Color(0xFFE3FBFF) //0XFFA29898
-                ),
-                elevation = CardDefaults.cardElevation(4.dp),
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(y = (-30.dp))
+                .height(300.dp)
+                .padding(horizontal = 36.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color(0xFFE3FBFF) //0XFFA29898
+            ),
+            elevation = CardDefaults.cardElevation(4.dp),
 //                shape = CircleShape,
 //                border = BorderStroke(2.dp, color = Color.Black)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
                     text = "Seus dados",
@@ -112,13 +130,15 @@ fun IMCScreen(name: String, modifier: Modifier = Modifier) {
                     fontWeight = FontWeight.Bold,
                     color = colorResource(R.color.app_col)
                 )
+                //Campo de input altura
                 OutlinedTextField(
-                    value = weightField,
+                    value = heightField,
                     onValueChange = { newInput ->
-                        weightField = newInput
+                        heightField = newInput
                     },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     label = {
-                        Text(text = "Altura")
+                        Text(text = "Altura(cm)")
                     },
                     singleLine = true,
                     shape = RoundedCornerShape(16.dp),
@@ -127,12 +147,102 @@ fun IMCScreen(name: String, modifier: Modifier = Modifier) {
                         focusedBorderColor = colorResource(R.color.app_col)
                     )
                 )
-//                TextField()
-//                Button() { }
+
+                //Campo de input peso
+                OutlinedTextField(
+                    value = weightField,
+                    onValueChange = { newInput ->
+                        weightField = newInput
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    label = {
+                        Text(text = "Peso(Kg)")
+                    },
+                    singleLine = true,
+                    shape = RoundedCornerShape(16.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = colorResource(R.color.app_col),
+                        focusedBorderColor = colorResource(R.color.app_col)
+                    )
+                )
+
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    onClick = {
+                        val imc = getImc(weightField = weightField, heightField = heightField)
+                        imcValue = String.format("%.1f", imc)
+                        imcMessage = classifyImc(imc)
+
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        colorResource(R.color.app_col)
+                    )
+                ) {
+                    Text(
+                        modifier = Modifier.padding(vertical = 6.dp),
+                        color = Color.White,
+                        text = "CALCULAR"
+                    )
+                }
             }
         }
 
-        // Card-resultado
-        //Row()
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(75.dp)
+                .padding(horizontal = 36.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color(95, 182, 82, 255) //0XFFA29898
+            ),
+            elevation = CardDefaults.cardElevation(4.dp),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                // IMC
+                IMCText(text = imcValue.toString())
+                IMCText(text = imcMessage)
+            }
+
+        }
     }
+}
+
+
+@Composable
+fun IMCText(text:String) {
+    Text(
+        text = text,
+        color = Color.White,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+
+}
+
+fun getImc(weightField:String, heightField:String):Double{
+    val weight = weightField.toDouble()
+    val height = heightField.toDouble()/100
+    return weight/(height*height)
+}
+
+fun classifyImc(imc:Double):String{
+    val imcMessage: String = when(imc){
+        in 0.0..<18.5 -> "Abaixo do peso."
+        in 18.5..< 25.0 -> "Peso ideal."
+        in 25.0..< 30.0 -> "Levemente acima do peso."
+        in 18.5..< 35.0 -> "Obesidade grau I."
+        in 18.5..< 40.0 -> "Obesidade grau II."
+        else if (imc > 40.0) -> "Obesidade grau III."
+        else -> "Alguma coisa deu errado"
+    }
+
+    return imcMessage
+
 }
